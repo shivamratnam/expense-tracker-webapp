7c6f9b4abdb1ac96533f4a2410a66937be0c972c
Shivamratnam Tiwari <shivamratnam.tiwari@gmail.com>
2025-09-27 15:19:20 +0530

    updated login and signup routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -8,7 +8,7 @@ import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
     {
-        path: '', component: HomeComponent
+        path: '', component: LoginComponent
     },
     {
         path: 'login', component: LoginComponent
